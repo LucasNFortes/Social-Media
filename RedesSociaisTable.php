f49e5fd77f73ddc5307d3209631253c798bab0ee
Lucas <111457694+LucasNFortes@users.noreply.github.com>
2023-02-02 20:18:47 -0400

    Add files via upload

---
+++ RedesSociaisTable.php
@@ -0,0 +1,90 @@
+<style>
+    table td {
+        border: 0;
+    }
+</style>
+<div>
+    <table>
+    <tr>
+        <td>
+            <div>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+                <a href="https://www.linkedin.com/in/lucasnfortes/" style="position:fixed; width:60px; height:60px; background-color:#0077B5; color:#FFF; border-radius:40px; text-align:center; font-size:30px; box-shadow: 1px 1px 2px #888; target="_blank">
+                <i style="margin-top:16px" class="fa fa-linkedin"></i>
+                </a>
+            </div>
+        </td>
+        <td>
+            <div>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+                <a href="https://github.com/LucasNFortes" style="position:fixed; width:60px; height:60px; background-color:#6879D9; color:#FFF; border-radius:40px; text-align:center; font-size:30px; box-shadow: 1px 1px 2px #888; target="_blank">
+                <i style="margin-top:16px" class="fa fa-github"></i>
+                </a>
+            </div>
+        </td>
+        <td>
+            <div>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+                <a href="https://www.instagram.com/lucas_nfortes/" style="position:fixed; width:60px; height:60px; background-color:#e1306c; color:#FFF; border-radius:40px; text-align:center; font-size:30px; box-shadow: 1px 1px 2px #888; target="_blank">
+                <i style="margin-top:16px" class="fa fa-instagram"></i>
+                </a>
+            </div>
+        </td>
+        <td>
+            <div>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+                <a href="https://www.youtube.com/channel/UCWYQIcnzM9BA0RyQWnqfzYw" style="position:fixed; width:60px; height:60px; background-color:#bb0000; color:#FFF; border-radius:40px; text-align:center; font-size:30px; box-shadow: 1px 1px 2px #888; target="_blank">
+                <i style="margin-top:16px" class="fa fa-youtube-play"></i>
+                </a>
+            </div>
+        </td>
+        <td>
+            <div>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+                <a href="https://web.whatsapp.com/send/?phone=5592999133135" style="position:fixed; width:60px; height:60px; background-color:#34ad23; color:#FFF; border-radius:40px; text-align:center; font-size:30px; box-shadow: 1px 1px 2px #888; target="_blank">
+                <i style="margin-top:16px" class="fa fa-whatsapp"></i>
+                </a>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td>
+        <a></a><!-Espaço->
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <div>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+                <a href="https://steamcommunity.com/profiles/76561198076723036/" style="position:fixed; width:60px; height:60px; background-color:#10293F; color:#FFF; border-radius:40px; text-align:center; font-size:30px; box-shadow: 1px 1px 2px #888; target="_blank">
+                <i style="margin-top:16px" class="fa fa-steam"></i>
+                </a>
+            </div>
+        </td>
+        <td>
+            <div>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+                <a href="https://open.spotify.com/user/lucasnfortes?si=f3f1815785b64ec6" style="position:fixed; width:60px; height:60px; background-color:#7ab800; color:#FFF; border-radius:40px; text-align:center; font-size:30px; box-shadow: 1px 1px 2px #888; target="_blank">
+                <i style="margin-top:16px" class="fa fa-spotify"></i>
+                </a>
+            </div>
+        </td>
+        <td>
+            <div>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+                <a href="https://www.facebook.com/lucasdonfortes/" style="position:fixed; width:60px; height:60px; background-color:#3b5998; color:#FFF; border-radius:40px; text-align:center; font-size:30px; box-shadow: 1px 1px 2px #888; target="_blank">
+                <i style="margin-top:16px" class="fa fa-facebook"></i>
+                </a>
+            </div>
+        </td>
+        <td>
+            <div>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+                <a href="https://www.redbubble.com/people/LucasNFortes/" style="position:fixed; width:60px; height:60px; background-color:#e21621; color:#FFF; border-radius:40px; text-align:center; font-size:30px; box-shadow: 1px 1px 2px #888; target="_blank">
+                <img style="margin-top:16px; width: 30px" src="http://localhost/wordpress/wp-content/uploads/2023/02/redbubble.png" alt="descrição da imagem aqui">
+                </a>
+            </div>
+        </td>
+    </tr>
+    </table>
+</div>
